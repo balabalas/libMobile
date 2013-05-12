c3b6343949fb05974e7251d209a02e834ee6e3b9
Allen Heavey <ista.allen@gmail.com>
2013-05-12 14:55:24 +0800

    add iscroll

---
+++ assets/www/js/list.js
@@ -0,0 +1,25 @@
+/***
+ * scroll list.
+ * 
+ * **/
+
+(function(){
+    
+    "use strict";
+    
+    var myScroll;
+    
+    function appready(){
+        myScroll = new iScroll('wrapper');
+    }
+    
+    
+    document.addEventListener('deviceready', appready);
+    
+})();
+
+
+
+
+
+
